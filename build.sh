05dad958a32b71a5fc8d0f546a1a22f56a66c2de
Christophe Demarey <christophe.demarey@inria.fr>
2019-01-16 09:55:30 +0100

    flush PL core dir value to do net get the one of the build server

--- build.sh
+++ build.sh
@@ -40,6 +40,8 @@ function run_tests() {
 
 function package_developer_version() {
 	./pharo PharoLauncher.image eval --save "PhLDirectoryBasedImageRepository location"
+        # Avoid to have PL core dir set to the slave location
+	./pharo PharoLauncher.image eval --save "PhLTemplateSources launcherCoreDir: nil"
 	./pharo PharoLauncher.image eval '(MBConfigurationRoot current configurationInfoFor: ConfigurationOfPharoLauncher) version versionNumber' > launcher-version.txt
 	set_env
 	zip -9r PharoLauncher-developer-$VERSION_NUMBER.zip PharoLauncher.image PharoLauncher.changes launcher-version.txt
